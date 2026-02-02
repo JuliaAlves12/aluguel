f4f9a15d855d11cdf4a3474d8f03ae74d544ac8f
Julia Alves Pereira <Juliaalvespereira01@gmail.com>
2026-02-02 11:17:10 -0300

    Atualizações

--- api/serializers.py
+++ api/serializers.py
@@ -5,25 +5,25 @@ class UsuarioSerializer(serializers.ModelSerializer):
     class Meta:
         model = Usuario
         fields = '__all__'
-        read_only_fields = ['id']
+        # read_only_fields = ['id']
 
 
 class ImovelSerializer(serializers.ModelSerializer):
     class Meta:
         model = Imovel
         fields = '__all__'
-        read_only_fields = ['id']
+        # read_only_fields = ['id']
 
 
 class PagamentoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Pagamento
         fields = '__all__'
-        read_only_fields = ['id']
+        # read_only_fields = ['id']
 
 
 class ContratoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Contrato
         fields = '__all__'
-        read_only_fields = ['id']
+        # read_only_fields = ['id']
--- api/views.py
+++ api/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
 from .models import Usuario, Imovel, Contrato, Pagamento
 from .serializers import UsuarioSerializer, ImovelSerializer, ContratoSerializer, PagamentoSerializer
-from rest_framework.decorators import api_view
 # Create your views here.
 
+class UsuarioListCreateAPIView(APIView):
+    def get(self, request):
+        usuarios = Usuario.objects.all()
+        serializer = UsuarioSerializer(usuarios, many=True)
+        return Response(serializer.data)
