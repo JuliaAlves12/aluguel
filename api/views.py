a632a29c5513ef99d8bcf7d22ed999c42a594266
Julia Alves Pereira <Juliaalvespereira01@gmail.com>
2026-02-02 11:23:58 -0300

    Atualizações

--- api/views.py
+++ api/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework.response import Response
+from rest_framework.generics import ListCreateAPIView
 from rest_framework.views import APIView
 from rest_framework import status
 from .models import Usuario, Imovel, Contrato, Pagamento
